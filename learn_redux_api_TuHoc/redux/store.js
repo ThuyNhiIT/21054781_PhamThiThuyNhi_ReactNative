da558984d4dbe32c4244daa3cc12ba8768c8061c
Thùy Nhi IT <21054781.nhi@student.iuh.edu.vn>
2024-11-10 17:07:26 +0700

    Revert "Tự học: Tìm hiểu cách triển khai redux với API"
    This reverts commit 747d0f9f75a36f453b12e8e425b5a05c856ac2fc.

--- learn_redux_api_TuHoc/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import todoReducer from './reducer';
-import thunk from 'redux-thunk';
-
-const store = createStore(todoReducer, applyMiddleware(thunk));
-
-export default store;
